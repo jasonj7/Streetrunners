112cfae6f5b20ed87c8bcf377687cbdabac190e4
Zoffy1966 <132029485+Zoffy1966@users.noreply.github.com>
2023-05-04 20:11:46 -0700

    PlayerControllerScript
    Added player controller script

---
+++ Streetrunners/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isOnGround;
+    public Rigidbody2D playerrb;
+    private float jumpTimer;
+    
+    [SerializeField]
+    private float speed;    
+    
+    [SerializeField]
+    private float timedJump;
+
+    [SerializeField]
+    private float minJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOnGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpTimer = Time.fixedTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && isOnGround && playerrb.velocity.y > -0.1f && playerrb.velocity.y < 0.1f)
+        {
+            jumpTimer = Time.fixedTime - jumpTimer;
+            if (jumpTimer > timedJump)
+            {
+                jumpTimer = timedJump;
+            }
+            playerrb.AddForce(Vector2.up * speed * (jumpTimer + minJump), ForceMode2D.Impulse);
+            
+        }
+    }
+}
